ffbcf24461dbbb648298fe5cf4b06f7bfa368119
Khatfield <kassidondoe@yahoo.com>
2022-08-19 18:25:01 +0000

    register.html

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 # Create your views here.
@@ -29,21 +30,22 @@ def login_user(request):
 
 
 def register_user(request):
-  if request.method  == "POST":
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      form.save()
-      username = form.cleaned_data['username']
-      password = form.cleaned_data['passwword1']
-      user = authenticate(username=username,password=password)
-      login(request,user)
-      messages.success(request, ('Login Success'))
-      return redirect('Navigation')
-  else:
-    form = UserCreationForm()
-  return render(request, 'authenticate/register_user.html',{'form':form,})
+	if request.method == "POST":
+		form = RegisterUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Registration Successful!"))
+			return redirect('home')
+	else:
+		form = RegisterUserForm()
 
-
+	return render(request, 'authenticate/register_user.html', {
+		'form':form,
+		})
 
 def Navigation(request):
   if request.method  == "POST":
